c456e53c35ab9932314c1b131a0db807ec98ed90
Spirtix <michal.prazak1@gmail.com>
2024-08-07 11:47:21 +0200

    init plugin

---
+++ src/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using HarmonyLib;
+using modoff.Runtime;
+using System;
+using UnityEngine;
+
+namespace modoff
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin {
+        public void Awake() {
+            Harmony harmony = new Harmony("SoDOff");
+
+            harmony.PatchAll();
+            Logger.LogInfo("SoDOff patch");
+        }
+    }
+}
